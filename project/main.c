53c13a53635d146dd0e15eee2fd7f44962c5a959
Zaphryx1 <lalo.gaytan@yahoo.com>
2024-11-10 21:46:06 -0700

    added main

---
+++ project/main.c
@@ -0,0 +1,29 @@
+#include <msp430.h>
+
+#include "libTimer.h"
+
+#include "led.h"
+
+#include "switches.h"
+
+#include "buzzer.h"
+
+
+
+/*this sets clocks, and interrupts */
+
+int main(void) {
+
+  configureClocks(); // setting CPU and clocks
+
+  switch_init();/* initiating switches */
+
+  buzzer_init(); /* initiating buzzer  */
+
+  led_init();
+
+
+
+  or_sr(0x18);  /* CPU off, GIE on */
+
+}
